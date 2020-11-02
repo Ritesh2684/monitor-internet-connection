e5761518933f507f76e1cbc39c926737e2c9b4f6
JB25TV <JB25TV@WPU8L0077928.ad.ing.net>
2020-11-02 13:44:43 +0100

    updated.

--- src/main/java/com/omnichannel/isp/monitor/model/InternetConnectionState.java
+++ src/main/java/com/omnichannel/isp/monitor/model/InternetConnectionState.java
@@ -2,14 +2,11 @@ package com.omnichannel.isp.monitor.model;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.Entity;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
 public class InternetConnectionState {
 	
 	private InternetConnectionStatus internetConnectionStatus;
